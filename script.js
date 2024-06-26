208a4db267e0c1f5c27afa25457b34aae408235b
Mert <156681170+Mert-409@users.noreply.github.com>
2024-06-26 22:35:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+const epostaile=document.getElementById('epostaile')
+const epostailegiris=document.getElementById('epostailegiris')
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
+
+epostaile.addEventListener('click',function(){
+    window.location.href = 'mail.html';
+})
+
+epostailegiris.addEventListener('click',function(){
+    window.location.href = 'mail.html';
+})
